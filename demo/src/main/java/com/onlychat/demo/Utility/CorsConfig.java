31017127ec94fc5ff284e26dfcd5bbefdf0b8ffe
longhakly <61673082+longhakly@users.noreply.github.com>
2024-06-24 23:43:30 +0700

    Update CorsConfig.java

--- demo/src/main/java/com/onlychat/demo/Utility/CorsConfig.java
+++ demo/src/main/java/com/onlychat/demo/Utility/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://onlychat.vercel.app/")
+                .allowedOrigins("https://onlychats.vercel.app/")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
